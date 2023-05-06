d29a060db257d586fafd81b2acc154dda5848990
h-adji <h-adji_tech@proton.me>
2023-05-06 15:24:41 +0100

    working version of the game of life

--- index.js
+++ index.js
@@ -1,33 +1,80 @@
-let game_state = false;
-function gen_table(size) {
-    const universe_table = document.querySelector(".universe table");
-    const cell = "<td class='dead'></td>"
-    const row = `
-            <tr>
-                ${cell.repeat(size)}
-            </tr>
-                `;
-    const table = row.repeat(size)
-    universe_table.innerHTML = table;
+let universe_size = 100;
+let htmlElements;
+let cells;
+let DEAD = 0;
+let ALIVE = 1;
 
-};
-const play_button = document.querySelector("#play-btn");
-const pause_button = document.querySelector("#pause-btn");
-const reset_button = document.querySelector("#reset-btn");
-play_button.addEventListener("click", () => game_state = true);
-pause_button.addEventListener("click", () => game_state = false);
-pause_button.addEventListener("click", () => {
-    game_state = false;
-    const cells = document.querySelectorAll("tr .alive");
-    for (let i = 0; i < cells.length; i++) {
-        cells[i].classList.remove('alive');
-        cells[i].classList.add('dead');
+function createField() {
+    htmlElements = [];
+    cells = [];
+    let table = document.getElementById('field');
+    for (let y = 0; y < universe_size; y++) {
+        let tr = document.createElement('tr');
+        let tdElements = [];
+        cells.push(new Array(universe_size).fill(DEAD));
+        htmlElements.push(tdElements);
+        table.appendChild(tr);
+        for (let x = 0; x < universe_size; x++) {
+            let td = document.createElement('td');
+            tdElements.push(td);
+            tr.appendChild(td);
+        }
     }
-});
+}
 
-gen_table(150);
-function render() {
-    while (game_state == true) {
-        // logic
+function draw() {
+    for (let y = 0; y < universe_size; y++) {
+        for (let x = 0; x < universe_size; x++) {
+            htmlElements[y][x].setAttribute('class', 'cell ' + (cells[y][x] == 1 ? 'filled' : 'empty'));
+        }
     }
-};
+}
+
+function countNeibhours(x, y) {
+    let count = 0;
+    for (dy = -1; dy <= 1; dy++) {
+        for (dx = -1; dx <= 1; dx++) {
+            let nx = (x + dx + universe_size) % universe_size, ny = (y + dy + universe_size) % universe_size;
+            count = count + cells[ny][nx];
+        }
+    }
+    return count - cells[y][x];
+}
+
+function newGeneration() {
+    let newCells = [];
+    for (let i = 0; i < universe_size; i++) {
+        newCells.push(new Array(universe_size).fill(DEAD));
+    }
+    for (let y = 0; y < universe_size; y++) {
+        for (let x = 0; x < universe_size; x++) {
+            let neibhours = countNeibhours(x, y);
+            if (cells[y][x] == DEAD && neibhours == 3) {
+                newCells[y][x] = ALIVE;
+            }
+            if (cells[y][x] == ALIVE && (neibhours == 2 || neibhours == 3)) {
+                newCells[y][x] = ALIVE;
+            }
+        }
+    }
+    cells = newCells;
+    draw();
+}
+
+function init() {
+    createField();
+    for (let i = 0; i < Math.floor(universe_size * universe_size * 0.3); i++) {
+        let x, y;
+        do {
+            x = Math.floor(Math.random() * universe_size), y = Math.floor(Math.random() * universe_size);
+            if (cells[y][x] == DEAD) {
+                cells[y][x] = ALIVE;
+                break;
+            }
+        } while (true);
+    }
+    draw();
+    setInterval(newGeneration, 500);
+}
+
+init();
